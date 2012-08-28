05adfd74b5835931c59770a90093849485fab292
Rob Mayoff <git@rob.dqd.com>
2012-08-28 18:08:31 -0500

    Initial Commit

---
+++ GraphView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GraphView
+//
+//  Created by Rob Mayoff on 8/28/12.
+//  Copyright (c) 2012 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GraphView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GraphView
+//
+//  Created by Rob Mayoff on 8/28/12.
+//  Copyright (c) 2012 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
